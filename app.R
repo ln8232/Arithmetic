f7108950cd8a26d9831b2745b5bfd170ec64ba90
Larry Nguyen <ln8232@utexas.edu>
2025-09-30 13:12:44 -0500

    =Modified files

--- app.R
+++ app.R
@@ -9,20 +9,43 @@ ui <- fluidPage(
                          value = 0),
             numericInput(inputId = "number2",
                          label = "Number 2",
-                         value = 0)
+                         value = 0),
+            actionButton(inputId = "add",
+                         label = "Add"),
+            actionButton(inputId = "multiply",
+                         label = "Multiply")
         ),
         mainPanel(
             h2("The sum of the two numbers is:"),
-            textOutput("sum")
+            textOutput("sum"),
+            h2("The product of the two numbers is:"),
+            textOutput("product")
         )
     )
 )
 
 server <- function(input, output) {
+    a <- eventReactive(input$add, {
+        as.numeric(input$number1)
+    })
+    b <- eventReactive(input$add, {
+        as.numeric(input$number2)
+    })
+    c <- eventReactive(input$multiply, {
+        as.numeric(input$number1)
+    })
+    d <- eventReactive(input$multiply, {
+      as.numeric(input$number2)
+    })
     output$sum <- renderText({
-        x1 <- as.numeric(input$number1)
-        x2 <- as.numeric(input$number2)
+        x1 <- a()
+        x2 <- b()
         x1 + x2
+    })
+    output$product <- renderText({
+        x1 <- c()
+        x2 <- d()
+        x1 * x2
     })
 }
 
